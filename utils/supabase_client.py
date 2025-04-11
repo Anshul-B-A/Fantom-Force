1df96ca69c392f80d8f01788701da65a6e5052f2
gaya3jayan-11 <gayatrijayan2003@gmail.com>
2025-04-11 15:10:53 +0530

    auth-enabled

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -1,10 +1,7 @@
+# utils/supabase_client.py
 from supabase import create_client, Client
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = "https://ixcpxraktziqeatwjvta.supabase.co"  
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4Y3B4cmFrdHppcWVhdHdqdnRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNjA2MjUsImV4cCI6MjA1OTkzNjYyNX0.fvAz1KxiOGTFxvIiKBGx-wwjrKlQ9YhN2S0HnNnnC2s"             
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
